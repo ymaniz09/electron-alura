c6edff8123217303e008c1e8650388c84f91e347
Ygor <ygor.maniz@gmail.com>
2019-05-22 02:30:08 -0300

    Populating about window

--- main.js
+++ main.js
@@ -16,11 +16,28 @@ app.on('window-all-closed', () => {
     app.quit();    
 });
 
+let aboutWindow = null;
+
 ipcMain.on('open-about-window', () => {
-    let aboutWindow = new BrowserWindow({
-        width: 300,
-        height: 200
-    });
+    if (aboutWindow == null) { 
+        aboutWindow = new BrowserWindow({
+            width: 300,
+            height: 220,
+            alwaysOnTop: true,
+            frame: false,
+            webPreferences: {
+                nodeIntegration: true
+              }
+        });
+
+        aboutWindow.on('closed', () => {
+            aboutWindow = null;
+        });
+    }
 
     aboutWindow.loadURL(`file://${__dirname}/app/about.html`);
+});
+
+ipcMain.on('close-about-window', () => {
+    aboutWindow.close();
 });
